88da2339025fc3d1c7af3d92c24c9da15a4c6041
ape-with-helmet <ganeshshatrugna@gmail.com>
2024-07-11 10:04:21 +0530

    Update

--- models/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-    print("File is corrupted or not found.")
--- models/test.py
+++
@@ -1,8 +0,0 @@
-import h5py
-try:
-    with h5py.File('facenet_keras.h5', 'r') as file:
-        # File is open and ready for reading
-        print("File is valid and open.")
-except IOError:
-    # File could not be opened or is corrupted
-    print("File is corrupted or not found.")
